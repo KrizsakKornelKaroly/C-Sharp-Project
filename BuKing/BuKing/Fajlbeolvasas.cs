4ae564d9148b75ec8ec332b734a1823a5fae5525
Acsbeni <acsbenjamin@turr.hu>
2025-01-29 11:27:52 +0100

    Sql supp

--- BuKing/BuKing/Fajlbeolvasas.cs
+++ BuKing/BuKing/Fajlbeolvasas.cs
@@ -4,13 +4,21 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace BuKing
 {
     internal class Fajlbeolvasas
     {
-        
 
+        private static string server = "localhost";
+        private static string database = "";
+        private static string user = "root";
+        private static string password = "";
+        private static List<Booking> orszagokList = new List<Booking>();
+
+        private static string connectionString = $"Server={server};Database={database};User ID={user};Password={password};";
+        public static MySqlConnection connection = new MySqlConnection(connectionString);
 
 
 
