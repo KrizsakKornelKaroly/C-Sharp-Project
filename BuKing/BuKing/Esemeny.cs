0af21e6a4b028ce48308f231c848162e7a7f967e
Acsbeni <acsbenjamin@turr.hu>
2025-01-31 17:04:50 +0100

    Tabla classok

--- BuKing/BuKing/Esemeny.cs
+++ BuKing/BuKing/Esemeny.cs
@@ -8,10 +8,10 @@ namespace BuKing
 {
     internal class Esemeny
     {
-        public int Esemeny_id;
-        public string Nev;
-        public DateTime Idopont;
-        public int Helyszin_id;
+        private int esemeny_id;
+        private string nev;
+        private DateTime idopont;
+        private int helyszin_id;
 
         public Esemeny(int esemeny_id, string nev, DateTime idopont, int helyszin_id)
         {
@@ -19,6 +19,16 @@ namespace BuKing
             Nev = nev;
             Idopont = idopont;
             Helyszin_id = helyszin_id;
+        }
+
+        public int Esemeny_id { get => esemeny_id; set => esemeny_id = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public DateTime Idopont { get => idopont; set => idopont = value; }
+        public int Helyszin_id { get => helyszin_id; set => helyszin_id = value; }
+
+        public override string ToString()
+        {
+            return $"{esemeny_id}, {nev}, {idopont}, {helyszin_id}";
         }
     }
 }
---
+++ BuKing/BuKing/Jegy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuKing
+{
+    internal class Jegy
+    {
+        private string jegytipus;
+        private int jegyar;
+        private int esemenyid;
+        private int mennyiseg;
+        private int jegyid;
+
+        public Jegy(string jegytipus, int jegyar, int esemenyid, int mennyiseg, int jegyid)
+        {
+            Jegytipus = jegytipus;
+            Jegyar = jegyar;
+            Esemenyid = esemenyid;
+            Mennyiseg = mennyiseg;
+            Jegyid = jegyid;
+        }
+
+        public string Jegytipus { get => jegytipus; set => jegytipus = value; }
+        public int Jegyar { get => jegyar; set => jegyar = value; }
+        public int Esemenyid { get => esemenyid; set => esemenyid = value; }
+        public int Mennyiseg { get => mennyiseg; set => mennyiseg = value; }
+        public int Jegyid { get => jegyid; set => jegyid = value; }
+
+        public override string ToString()
+        {
+            return $"{jegytipus}, {jegyar}, {esemenyid}, {mennyiseg}, {jegyid}";
+        }
+    }
+}
